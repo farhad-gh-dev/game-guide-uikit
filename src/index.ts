17acb03863c9ac9f45d20c0dd9c351458e946372
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-04-18 23:01:35 +0430

    Update src index

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import Avatar from "./Components/Avatar/Avatar";
+import BrandLogo from "./Components/BrandLogo/BrandLogo";
+import Button from "./Components/Button/Button";
+import CustomIcon from "./Components/CustomIcon/CustomIcon";
+import Dropdown from "./Components/Dropdown/Dropdown";
+import Heading from "./Components/Heading/Heading";
+import ItemCard from "./Components/ItemCard/ItemCard";
+import { Navbar, NavbarItem } from "./Components/Navbar/Navbar";
+import Rating from "./Components/Rating/Rating";
+import SliderControl from "./Components/SliderControl/SliderControl";
+import Text from "./Components/Text/Text";
+
+import * as styles from "./Styles/styles";
+import * as global from "./Styles/global";
+
+export {
+    styles, 
+    global,
+    Avatar,
+    BrandLogo,
+    Button,
+    CustomIcon,
+    Dropdown,
+    Heading,
+    ItemCard,
+    Navbar, NavbarItem,
+    Rating,
+    SliderControl,
+    Text
+};
