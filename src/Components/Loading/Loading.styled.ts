4797a94094ed1bdff5457b314725ad8f706e0502
Farhad Gholami <farhad.dev.contact@gmail.com>
2022-08-04 13:00:53 +0430

    Update loading component style

--- src/Components/Loading/Loading.styled.ts
+++ src/Components/Loading/Loading.styled.ts
@@ -6,6 +6,8 @@ export const StyledLoading = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  width: 100%;
+  height: 100%;
 
   svg {
     position: absolute;
